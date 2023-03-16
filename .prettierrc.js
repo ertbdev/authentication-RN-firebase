2aa890f0a6a75e2972a234e85bc9770b276fd103
Ricardo Toledo <ingeniero.ricardo.toledo@gmail.com>
2023-03-16 19:37:03 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  printWidth: 140,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {SafeAreaView, StatusBar, StyleSheet, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={[styles.container, backgroundStyle]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={backgroundStyle.backgroundColor} />
+
+      <Text style={styles.title}>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: '#000',
+  },
+});
+
+export default App;
