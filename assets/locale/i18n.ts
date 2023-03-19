8872fa75de9af4a93ca1e6533502c97e234a116a
Ricardo Toledo <ingeniero.ricardo.toledo@gmail.com>
2023-03-19 10:49:57 +0100

    added and configured i18n-js for handling translations

---
+++ assets/locale/i18n.ts
@@ -0,0 +1,12 @@
+import {I18n} from 'i18n-js';
+import en from './en.json';
+import es from './es.json';
+import pl from './pl.json';
+
+const i18n = new I18n({
+  en: en,
+  es: es,
+  pl: pl,
+});
+
+export default i18n;
--- screens/LandingScreen.tsx
+++ screens/LandingScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useTheme} from 'styled-components/native';
+import i18n from '../assets/locale/i18n';
 import {Colors} from '../styles/themes/types';
 
 const LandingScreen = () => {
@@ -10,7 +11,7 @@ const LandingScreen = () => {
   return (
     <SafeAreaView style={styles.safeArea} edges={['left', 'right', 'top']}>
       <View style={styles.container}>
-        <Text style={styles.title}>Hello world</Text>
+        <Text style={styles.title}>{i18n.t('hello')}</Text>
       </View>
     </SafeAreaView>
   );
